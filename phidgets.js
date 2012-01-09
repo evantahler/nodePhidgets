66145eeef3f4be1d56e74be7755e8595f8ed636d
Evan Tahler <evantahler@gmail.com>
2012-01-09 07:36:59 -0800

    .

--- phidgets.js
+++ phidgets.js
@@ -25,19 +25,21 @@ phidgets.connect = function(params, next){
 	console.log(params);
 	
 	phidgets.client = phidgets.net.createConnection(params.port, params.host, function(){
-		phidgets.client.setEncoding('utf8');
-		phidgets.client.setKeepAlive("enable", 10000)
-		phidgets.client.on('data', phidgets.handleData);
-		phidgets.client.on('end', phidgets.handleConnectionEnd);
-		phidgets.client.on('close', phidgets.handleConnectionEnd);
-		phidgets.client.on('error', phidgets.handleError);
+		var conn = phidgets.client;
+		conn.setEncoding('utf8');
+		conn.setKeepAlive("enable", 10000)
+		conn.on('data', phidgets.handleData);
+		conn.on('end', phidgets.handleConnectionEnd);
+		conn.on('close', phidgets.handleConnectionEnd);
+		conn.on('error', phidgets.handleError);
 		
-		phidgets.client.write("need nulls");
-		phidgets.client.write("995 authenticate, version="+params.version); // 995 authenticate, version=1.0.9
-		phidgets.client.write("report 8 report");
-		phidgets.client.write("set /PCK/Client/0.0.0.0/" + Math.random() + "/PhidgetInterfaceKit=\"Open\" for session"); // set /PCK/Client/0.0.0.0/123/PhidgetInterfaceKit="Open" for session
-		phidgets.client.write("listen /PSK/PhidgetInterfaceKit lid0");
-		next(phidgets.client);
+		conn.write("need nulls\r\n");
+		conn.write("995 authenticate, version="+params.version+"\r\n"); // 995 authenticate, version=1.0.9
+		conn.write("report 8 report\r\n");
+		conn.write("set /PCK/Client/0.0.0.0/" + Math.random() + "/PhidgetInterfaceKit=\"Open\" for session\r\n"); // set /PCK/Client/0.0.0.0/123/PhidgetInterfaceKit="Open" for session
+		conn.write("listen /PSK/PhidgetInterfaceKit lid0\r\n");
+		
+		next(conn);
 	});
 };
 
