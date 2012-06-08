b64c018d1971c31ac22ad79958f70932a7b76b8a
Brandon Miller <brandon@1942design.com>
2012-06-07 21:34:32 -0700

    Basically rewrote the module
    I was really inspired by the first version, but after quite a while of
    trying to get my RFID reader working, I realized, the original only
    supported the interfacekit.
    
    I allowed for suport of any phidget, and made the module much more event
    driven. I also allowed for modifying any possible paramater on any type
    of phidget... I think ;)

--- test.js
+++ test.js
@@ -1,54 +1,74 @@
-////////////////////////////////////////////////////////////////////////////
-// IMPORT
-var phidgets = require('./phidgets.js').phidgets;
+var phidget = require('phidgetAPI').phidget;
 
-////////////////////////////////////////////////////////////////////////////
-// SETUP EVENT LISTNERS (optional)
-var ready = false;
-phidgets.on("data", function(type, id, value){
-	if (ready){
-		console.log(" phidgets data >> "+type+" #"+id+" now at @ "+value);
-	}
-});
-phidgets.on("log", function(data){
-	console.log(" phidgets log >> "+data);
-});
-phidgets.on("error", function(e){
-	console.log(" phidgets error >> "+e);
-});
+phidget.on(
+    "log", 
+    function(data){
+        console.log('log ',data);
+    }
+);
+
+phidget.on(
+    "error", 
+    function(data){
+        console.log('error ',data);
+    }
+);
+
+phidget.on(
+    'changed', 
+    function(data){
+        console.log('changed');
+        console.log('data ',data);
+        console.log('new phidgets data ',phidget.data);
+        
+        switch(data.key){
+            case 'TagState' :
+                if(data.value=='1'){
+                    phidget.set(
+                        {
+                            type:'board',
+                            key:'LEDOn',
+                            value:'1'
+                        }
+                    );
+                    break;
+                }
+                
+                phidget.set(
+                    {
+                        type:'board',
+                        key:'LEDOn',
+                        value:'0'
+                    }
+                );
+                break;
+        }
+    }
+);
+    
+phidget.on(
+    'added', 
+    function(data){
+        console.log('added');
+        console.log('data ',data);
+        console.log('new phidgets data ',phidget.data);
+    }
+);
+
+phidget.on(
+    'phidgetReady',
+    function(){
+        console.log('phidget ready');
+        console.log(phidget.data);
+    }
+);
 
 
-////////////////////////////////////////////////////////////////////////////
-// PARAMS TO CONNECT TO YOUR PHIDGET BOARD (defaults will be used if none provided)
-var params = {
-	host: "phidgetsbc.local",
-};
-
-////////////////////////////////////////////////////////////////////////////
-// CONNECT AND GAIN ACCESS TO PHIDGET DATA OBJECT
-phidgets.connect(params, function(phidgetData){
-	ready = true;
-	console.log("All Connected!  Here's the array of data about inputs/outputs:");
-	console.log(phidgetData);
-	setTimeout(a, 1000);
-});
-
-////////////////////////////////////////////////////////////////////////////
-// CHANGE SOME OF THE OUTPUTS
-function a(){
-	phidgets.setOutput(4,true); // turn digital output #4 on
-	setTimeout(b, 1000);
-}
-
-function b(p){
-	phidgets.setOutput(4,false); // turn digital output #4 off
-	setTimeout(c, 1000);
-}
-
-////////////////////////////////////////////////////////////////////////////
-// DISCONNECT
-function c(){
-	phidgets.quit(function(){
-		console.log("ALL DONE!");
-	});
-}
+/*
+ * Connect to phidget 
+ */
+phidget.connect(
+    {
+        type:'PhidgetRFID'
+    }
+);
