99e89b91b82671909ed12b02312d02076c990ce0
Jean-Philippe Côté <jp@cote.cc>
2015-02-27 11:39:58 -0500

    Adjusted to match new syntax.

--- example.js
+++ example.js
@@ -1,26 +1,26 @@
-var phidgetsPrototype = require('./lib/phidgets.js');
+var phidgets = require('./lib/phidgets.js');
 var options = {
   host: "phidgetsbc.local"
-}
-var phidgets = new phidgetsPrototype(options);
+};
+var phidget = new phidgets.Phidget(options);
 
 // events
-phidgets.on('state',  function(state){ console.log("[state] " + state); });
-phidgets.on('error',  function(error){ console.log("[error] " + error); });
-phidgets.on('input',  function(boardId, id, value){ console.log("[" + boardId + "][input]  " + id + " @ " + value); });
-phidgets.on('sensor', function(boardId, id, value){ console.log("[" + boardId + "][sensor] " + id + " @ " + value); });
-phidgets.on('output', function(boardId, id, value){ console.log("[" + boardId + "][output] " + id + " @ " + value); });
+phidget.on('state',  function(state){ console.log("[state] " + state); });
+phidget.on('error',  function(error){ console.log("[error] " + error); });
+phidget.on('input',  function(boardId, id, value){ console.log("[" + boardId + "][input]  " + id + " @ " + value); });
+phidget.on('sensor', function(boardId, id, value){ console.log("[" + boardId + "][sensor] " + id + " @ " + value); });
+phidget.on('output', function(boardId, id, value){ console.log("[" + boardId + "][output] " + id + " @ " + value); });
 
 // debugging event
-// phidgets.on('line', function(line){ console.log("[line] " + line); });
+// phidget.on('line', function(line){ console.log("[line] " + line); });
 
 // connect
-phidgets.connect(function(){
+phidget.connect(function(){
 
   console.log("connected to:");
-  console.log(phidgets.ids);
+  console.log(phidget.ids);
 
-  setInterval(function(){ phidgets.setOutput(null, 0, true); }, 1000);
-  setInterval(function(){ phidgets.setOutput(null, 0, false);}, 1500);
+  setInterval(function(){ phidget.setOutput(null, 0, true); }, 1000);
+  setInterval(function(){ phidget.setOutput(null, 0, false);}, 1500);
 
 });
