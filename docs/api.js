be5ddbe7cc7420b77016744f7a0cf4fcd599e93c
= <jp@cote.cc>
2015-03-01 00:56:06 -0500

    Generated yuidoc documentation.

---
+++ docs/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Phidget",
+        "PhidgetInterfaceKit",
+        "PhidgetLED"
+    ],
+    "modules": [
+        "phidgets"
+    ],
+    "allModules": [
+        {
+            "displayName": "phidgets",
+            "name": "phidgets",
+            "description": "The `Phidgets` library module makes it easy to connect to a variety of boards made by\n[Phidgets Inc.](http://www.phidgets.com)"
+        }
+    ]
+} };
+});
