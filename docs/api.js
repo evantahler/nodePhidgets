ec096d657d1d66f4903a8841d2a01280e14cfcc5
= <jp@cote.cc>
2015-11-06 14:23:22 -0500

    Added support for the PhidgetBridge module.

--- docs/api.js
+++ docs/api.js
@@ -2,6 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Phidget",
+        "PhidgetBridge",
         "PhidgetInterfaceKit",
         "PhidgetLED"
     ],
